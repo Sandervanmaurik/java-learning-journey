c9316827465e8711990a8ee2b78ae70cf72d1dd9
Sandervanmaurik <sander.vm@hotmail.com>
2026-01-19 13:29:14 +0100

    Added test

--- learningjourney/src/main/java/com/sander/learningjourney/LearningjourneyApplication.java
+++ learningjourney/src/main/java/com/sander/learningjourney/LearningjourneyApplication.java
@@ -1,14 +1,12 @@
 package com.sander.learningjourney;
 
-import com.sander.learningjourney.controllers.MovieController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningjourneyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LearningjourneyApplication.class, args);
-    }
+     public static void main(String[] args) {
+         SpringApplication.run(LearningjourneyApplication.class, args);
+     }
 }
